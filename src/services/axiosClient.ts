3cf4f33bca4571d911699f80fbd0eb1fa12c8997
LuanVM1810 <Vomongluan@gmail.com>
2024-06-13 22:16:12 +0700

    add structure

---
+++ src/services/axiosClient.ts
@@ -0,0 +1,35 @@
+import axios, { AxiosError } from "axios";
+// import { isEmpty } from "lodash";
+
+const axiosClient = axios.create({
+  baseURL: "https://localhost:7158/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+axiosClient.interceptors.request.use(
+  function (config) {
+    // const token = localStorage.getItem("token");
+    // // Do something before request is sent
+    // if (token !== null) {
+    //   console.log("Token: ", token);
+    //   config.headers["Authorization"] = `Bearer ${token}`;
+    // }
+    return config;
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
+axiosClient.interceptors.response.use(
+  (response) => {
+    return response.data;
+  },
+  (error: AxiosError<{ content: string }>) => {
+    return error.response?.data;
+  }
+);
+
+export default axiosClient;
