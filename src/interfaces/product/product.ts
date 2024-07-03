e0510f948b48b1ac7ea4215c383b579781ce5c44
Huy-techSw <lequanghuy30012003@gmail.com>
2024-07-03 20:20:45 +0700

    Fixing product

--- src/interfaces/product/product.ts
+++ src/interfaces/product/product.ts
@@ -3,23 +3,26 @@ export const products = [
   {
     ProductID: 1,
     title: "Product 1",
-    description: "Description for product 1",
+    description: "Quick",
     price: 100,
-    imageURL: "https://via.placeholder.com/200",
+    imageURL:
+      "https://i.pinimg.com/originals/1e/11/9b/1e119b0de1b5ea18553c528e3ddb92b2.jpg",
   },
   {
     ProductID: 2,
     title: "Product 2",
-    description: "Description for product 2",
+    description: "product 2",
     price: 200,
-    imageURL: "https://via.placeholder.com/200",
+    imageURL:
+      "https://i.pinimg.com/originals/1e/11/9b/1e119b0de1b5ea18553c528e3ddb92b2.jpg",
   },
   {
     ProductID: 3,
     title: "Product 3",
-    description: "Description for product 3",
+    description: "product 3",
     price: 300,
-    imageURL: "https://via.placeholder.com/200",
+    imageURL:
+      "https://i.pinimg.com/originals/1e/11/9b/1e119b0de1b5ea18553c528e3ddb92b2.jpg",
   },
 ];
 
